0d9c1c2c6fbb4dee0591a764f10df5e1093a7676
saimaYar <61507198+saimaYar@users.noreply.github.com>
2020-05-01 01:13:31 +0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASSIGNMENT3.BAL;
+using ASSIGNMENT3.Entities;
+
+namespace EAD_ASSIGNMENT3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetFolders(int uid, int parentFolderId)
+        {
+            List<folderDTO> foldersList = folderBO.getFolders(uid, parentFolderId);
+            var data = new
+            {
+                folders = foldersList,
+            };
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult CreateFolders(String child, int uid, int parentFolder)
+        {
+            int id = 0;
+            bool flag = false;
+            folderDTO folder = folderBO.createFolder(child, uid, parentFolder);
+            if (folder != null)
+            {
+                id = folder.folderId;
+                flag = true;
+            }
+            var data = new
+            {
+                success = flag,
+                folderId = id
+            };
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ userController.cs
@@ -0,0 +1,83 @@
+﻿using ASSIGNMENT3.BAL;
+using ASSIGNMENT3.Entities;
+using EAD_ASSIGNMENT3.security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EAD_ASSIGNMENT3.Controllers
+{
+    public class userController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ValidateUser(String Login, String Password)
+        {
+            userDTO userInfo = userBO.validateUser(Login, Password);
+            bool flag = false;
+            if (userInfo != null)
+            {
+                sessionManager.User = userInfo;
+                flag = true;
+            }
+            var data = new
+            {
+                success = flag
+            };
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterUser(userDTO userInfo)
+        {
+            int id = userBO.save(userInfo);
+            bool flag = true;
+            if (id == 0)
+            {
+                flag = false;
+            }
+            else
+            {
+                userInfo.id = id;
+                sessionManager.user = userInfo;
+            }
+            var data = new
+            {
+                success = flag
+            };
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Home()
+        {
+            if (sessionManager.IsValidUser)
+            {
+                return View();
+            }
+            else
+            {
+                return Redirect("~/User/Login");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            sessionManager.ClearSession();
+            return Redirect("~/User/Login");
+        }
+    }
+}
