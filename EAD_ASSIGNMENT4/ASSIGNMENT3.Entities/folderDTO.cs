f0e3f435428e5b6e5c404f0f6f5cebc024ad1e69
saima <bsef17m020@pucit.edu.pk>
2020-05-01 17:20:21 +0500

    11

---
+++ EAD_ASSIGNMENT4/ASSIGNMENT3.Entities/folderDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASSIGNMENT3.Entities
+{
+    class folderDTO
+    {
+        public int folderId { get; set; }
+
+        public String folderName { get; set; }
+
+        public int parentFolderId { get; set; }
+
+        public int id { get; set; }
+    }
+}
---
+++ EAD_ASSIGNMENT4/EAD_ASSIGNMENT3/security/sessionManager.cs
@@ -0,0 +1,46 @@
+﻿using ASSIGNMENT3.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EAD_ASSIGNMENT3.security
+{
+    public class sessionManager
+    {
+        public static userDTO User
+        {
+            get
+            {
+                userDTO user = null;
+                if (HttpContext.Current.Session["user"] != null)
+                {
+                    user = HttpContext.Current.Session["user"] as userDTO;
+                }
+                return user;
+            }
+            set
+            {
+                HttpContext.Current.Session["user"] = value;
+            }
+        }
+
+        public static bool IsValidUser
+        {
+            get
+            {
+                if (User != null)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public static void ClearSession()
+        {
+            HttpContext.Current.Session.RemoveAll();
+            HttpContext.Current.Session.Abandon();
+        }
+    }
+}
